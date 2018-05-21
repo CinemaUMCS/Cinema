eb4833c5da342a5e0133c7466788ae7818408d5f
Pawel Sosnowka <pawel.sosnowka@outlook.com>
2018-05-21 21:49:53 +0200

    dodanie forgot password

--- Cinema/src/app/shared/user-api.service.ts
+++ Cinema/src/app/shared/user-api.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {BaseHttpService} from './base-http.service';
 import {Headers, Http} from '@angular/http';
 import {AuthenticationService} from './authentication.service';
+import {ChangePasswordModel} from '../../model/changePassword.model';
 
 @Injectable()
 export class UserApiService extends BaseHttpService {
@@ -13,6 +14,14 @@ export class UserApiService extends BaseHttpService {
   getActualUser() {
     const header = new Headers({'authorization': this.authenticationService.getToken()});
     return this.http.get(this.setUrl('account/details'), {headers: header});
+  }
+
+  changePassword(changePasswordModel: ChangePasswordModel) {
+    const header = new Headers({'Authorization': this.authenticationService.getToken()});
+    return this.http.put(this.setUrl('account/changePassword'), changePasswordModel, {headers: header});
+  }
+  forgotPassword(email: string){
+
   }
 
 }
